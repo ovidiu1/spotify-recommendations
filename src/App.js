5f6b4bcde3b740bb5f9ee3f8b694995c2820fc4f
ovidiu <oleaovidiu94@gmail.com>
2019-03-22 19:35:58 +0200

    develop new components & update project structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import GenresBar from './containers/GenresBar/GenresBar'
+import './App.css'
+class App extends Component {
+  render() {
+    return (
+      <>
+        <GenresBar />
+      </>
+    );
+  }
+}
+
+export default App
---
+++ src/containers/GenresBar/GenresBar.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import { withStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import TabContainer from '../../components/TabContainer/TabContainer';
+
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+export class GenresBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: 0,
+    }
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+    render() {
+      // const { classes } = this.props;
+      const { value } = this.state;
+    return (
+      <div>
+      <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+          >
+            <Tab label="Item One" />
+            <Tab label="Item Two" />
+            <Tab label="Item Three" />
+            <Tab label="Item Four" />
+            <Tab label="Item Five" />
+            <Tab label="Item Six" />
+            <Tab label="Item Seven" />
+          </Tabs>
+        </AppBar>
+        {value === 0 && <TabContainer>Item One</TabContainer>}
+        {value === 1 && <TabContainer>Item Two</TabContainer>}
+        {value === 2 && <TabContainer>Item Three</TabContainer>}
+        {value === 3 && <TabContainer>Item Four</TabContainer>}
+        {value === 4 && <TabContainer>Item Five</TabContainer>}
+        {value === 5 && <TabContainer>Item Six</TabContainer>}
+        {value === 6 && <TabContainer>Item Seven</TabContainer>}
+        
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(GenresBar)
