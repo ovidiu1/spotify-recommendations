193d687bf71302a750935b0e60d614d446e0ffd8
ovidiu <oleaovidiu94@gmail.com>
2019-03-22 12:43:41 +0200

    initial draft

---
+++ src/containers/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h2>Main Component</h2>
+      </>
+    );
+  }
+}
+
+export default App;
