5d633ffad225ca2e7d61a1053c978f05d39950b9
ovidiu <oleaovidiu94@gmail.com>
2019-03-23 14:07:45 +0200

    add axios / call spotify api / send data to child component

--- src/components/Crads/MediaCard.js
+++ src/components/Crads/MediaCard.js
@@ -6,70 +6,96 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import SkipNextIcon from '@material-ui/icons/SkipNext';
+
 
 const styles = theme => ({
-    card: {
-      display: 'flex',
-    },
-    details: {
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    content: {
-      flex: '1 0 auto',
-    },
-    cover: {
-      width: 151,
-    },
-    controls: {
-      display: 'flex',
-      alignItems: 'center',
-      paddingLeft: theme.spacing.unit,
-      paddingBottom: theme.spacing.unit,
-    },
-    playIcon: {
-      height: 38,
-      width: 38,
-    },
-  });
+  card: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+});
 
 const MediaCard = (props) => {
-    const { classes, theme } = props;
-    return (
-        <>
-            <Card className={classes.card}>
-      <div className={classes.details}>
-        <CardContent className={classes.content}>
-          <Typography component="h5" variant="h5">
-            Live From Space
-          </Typography>
-          <Typography variant="subtitle1" color="textSecondary">
-            Mac Miller
-          </Typography>
-        </CardContent>
-        <div className={classes.controls}>
-          {/* <IconButton aria-label="Previous">
-            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-          </IconButton> */}
-          <IconButton aria-label="Play/pause">
-            <PlayArrowIcon className={classes.playIcon} />
-          </IconButton>
-          {/* <IconButton aria-label="Next">
-            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-          </IconButton> */}
-        </div>
-      </div>
-      <CardMedia
-        className={classes.cover}
-        image="http://static1.squarespace.com/static/5957f2a8893fc0dd4859649f/5a6e3e0fe4966b35d920f21f/5c24fae321c67c6c46cc5b78/1545928799292/IMG_2096.JPG?format=1500w"
-        title="Live from space album cover"
-      />
-    </Card>
-        </>
-    )
+  const { classes, theme, tracks } = props;
+  console.info("data from MediaCard", tracks);
+
+  return (
+    <React.Fragment>
+      {props.tracks.map(track => (
+        <Card key={track.id} className={classes.card}>
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <Typography component="h5" variant="h5">
+                Live From Space
+              </Typography>
+              <Typography variant="subtitle1" color="textSecondary">
+                Mac Miller
+              </Typography>
+            </CardContent>
+            <div className={classes.controls}>
+              <IconButton aria-label="Play/pause">
+                <PlayArrowIcon className={classes.playIcon} />
+              </IconButton>
+            </div>
+          </div>
+          <CardMedia
+            className={classes.cover}
+            image="http://static1.squarespace.com/static/5957f2a8893fc0dd4859649f/5a6e3e0fe4966b35d920f21f/5c24fae321c67c6c46cc5b78/1545928799292/IMG_2096.JPG?format=1500w"
+            title="Live from space album cover"
+          />
+        </Card>
+      ))}
+    </React.Fragment>
+    // <React.Fragment>
+    //   <Card className={classes.card}>
+    //     <div className={classes.details}>
+    //       <CardContent className={classes.content}>
+    //         <Typography component="h5" variant="h5">
+    //           Live From Space
+    //       </Typography>
+    //         <Typography variant="subtitle1" color="textSecondary">
+    //           Mac Miller
+    //       </Typography>
+    //       </CardContent>
+    //       <div className={classes.controls}>
+
+    //         <IconButton aria-label="Play/pause">
+    //           <PlayArrowIcon className={classes.playIcon} />
+    //         </IconButton>
+
+    //       </div>
+    //     </div>
+    //     <CardMedia
+    //       className={classes.cover}
+    //       image="http://static1.squarespace.com/static/5957f2a8893fc0dd4859649f/5a6e3e0fe4966b35d920f21f/5c24fae321c67c6c46cc5b78/1545928799292/IMG_2096.JPG?format=1500w"
+    //       title="Live from space album cover"
+    //     />
+    //   </Card>
+    // </React.Fragment>
+  )
+}
+MediaCard.propTypes = {
+  tracks: PropTypes.array
 }
 
 export default withStyles(styles)(MediaCard)
--- src/containers/GenresBar/GenresBar.js
+++ src/containers/GenresBar/GenresBar.js
@@ -4,7 +4,9 @@ import { withStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import TabContainer from '../../components/TabContainer/TabContainer';
+import MediaCard from '../../components/Crads/MediaCard';
 
+import axios from 'axios';
 
 const styles = theme => ({
   root: {
@@ -14,48 +16,113 @@ const styles = theme => ({
   },
 });
 
+const genres = [
+  {
+    id: 0,
+    value: 'balck-metal',   
+  },  
+  {
+    id: 1,
+    value: 'deep-house',
+  },
+  {
+    id: 2,
+    value: 'drum-and-bass',
+  },
+  {
+    id: 3,
+    value: 'electro',
+  }
+]
+
 export class GenresBar extends Component {
   constructor(props){
     super(props);
     this.state = {
       value: 0,
+      seedGenres: '',
+      tracks: [],
+      isLoading: false,
+      error: null
     }
+  }
+  
+  getData = () => {
+    const token = '';
+    const config = {
+      headers: { 'Authorization': "Bearer " + token }
+    }
+
+    const endPoint = 'https://api.spotify.com/v1/recommendations';
+    const limit = 10;
+    let link = `${endPoint}?limit=${limit}&seed_genres=${this.state.seedGenres}`;
+    console.info(link);
+
+    axios.get(link, config) 
+    .then(result => this.setState({
+      tracks: result.data.tracks,
+    }))
+    .catch(error => this.setState({
+      error: true,
+      isLoading: false
+    }));
+  }
+  
+  selectGenres = () => { 
+     for(let i = 0; i < genres.length; i++ ) {
+       if (genres[i].id === this.state.value) {
+           this.setState({ seedGenres: genres[i].value })
+       }
+       alert(this.state.seedGenres);
+     }
   }
 
   handleChange = (event, value) => {
     this.setState({ value });
+    this.selectGenres();
+    this.getData();
   };
 
+
     render() {
-      // const { classes } = this.props;
-      const { value } = this.state;
+      const { value, tracks } = this.state;
+      console.info('track',tracks);
+
     return (
       <div>
       <AppBar position="static" color="default">
           <Tabs
             value={value}
             onChange={this.handleChange}
+            // onClick={this.selectGenres}
             indicatorColor="primary"
             textColor="primary"
             variant="scrollable"
             scrollButtons="auto"
           >
-            <Tab label="Item One" />
-            <Tab label="Item Two" />
-            <Tab label="Item Three" />
-            <Tab label="Item Four" />
+            <Tab label="Rock" />
+            <Tab label="House" />
+            <Tab label="Drum and Base" />
+            <Tab label="Electro" />
             <Tab label="Item Five" />
             <Tab label="Item Six" />
             <Tab label="Item Seven" />
           </Tabs>
         </AppBar>
-        {value === 0 && <TabContainer>Item One</TabContainer>}
+        {value === 0 && <MediaCard tracks={tracks}></MediaCard>}
+        {value === 1 && <MediaCard tracks={tracks}></MediaCard>}
+        {value === 2 && <MediaCard tracks={tracks}></MediaCard>}
+        {value === 3 && <MediaCard tracks={tracks}></MediaCard>}
+        {value === 4 && <MediaCard tracks={tracks}></MediaCard>}
+        {value === 5 && <MediaCard tracks={tracks}></MediaCard>}
+        {value === 6 && <MediaCard tracks={tracks}></MediaCard>}
+        {/* {value === 0 && <TabContainer>Item One</TabContainer>}
         {value === 1 && <TabContainer>Item Two</TabContainer>}
         {value === 2 && <TabContainer>Item Three</TabContainer>}
         {value === 3 && <TabContainer>Item Four</TabContainer>}
         {value === 4 && <TabContainer>Item Five</TabContainer>}
         {value === 5 && <TabContainer>Item Six</TabContainer>}
-        {value === 6 && <TabContainer>Item Seven</TabContainer>}
+        {value === 6 && <TabContainer>Item Seven</TabContainer>} */}
         
       </div>
     )
