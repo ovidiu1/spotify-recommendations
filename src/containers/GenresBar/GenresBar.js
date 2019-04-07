d499c00b856111618d328667d4410f430d1487f5
ovidiu <oleaovidiu94@gmail.com>
2019-04-07 12:01:45 +0300

    deploy09

--- src/containers/GenresBar/GenresBar.js
+++ src/containers/GenresBar/GenresBar.js
@@ -40,8 +40,30 @@ export class GenresBar extends Component {
   }
 
   getData = () => {
-    const token = process.env.REACT_APP_ACCESS_TOKEN;
-    console.log("FE TOKEN", token)
+
+    function getHashParams() {
+      var hashParams = {};
+      var e, r = /([^&;=]+)=?([^&;]*)/g,
+          q = window.location.hash.substring(1);
+      while ( e = r.exec(q)) {
+         hashParams[e[1]] = decodeURIComponent(e[2]);
+      }
+      return hashParams;
+    }
+    var params = getHashParams();
+            var access_token = params.access_token,
+            refresh_token = params.refresh_token,
+            error = params.error;
+
+    if (error) {
+      alert('There was an error during the authentication');
+    } else {
+      if (access_token) {
+        // render oauth info
+          console.log('All good!');
+      }
+      const token = access_token;
+
     const config = {
       headers: { 'Authorization': "Bearer " + token }
     }
@@ -61,6 +83,7 @@ export class GenresBar extends Component {
       isLoading: false
     }));
   }
+}
 
   handleChange = (event, value) => {
     this.setState({ value },() => {
