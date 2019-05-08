28043c110ece3ee4e6197e277a0c9faca3776d06
ovidiu <oleaovidiu94@gmail.com>
2019-05-08 22:04:23 +0300

    refactor

--- src/containers/GenresBar/GenresBar.js
+++ src/containers/GenresBar/GenresBar.js
@@ -3,10 +3,6 @@ import AppBar from '@material-ui/core/AppBar';
 import {  withStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Grid from '@material-ui/core/Grid';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import ErrorComponent from '../../components/Error/ErrorComponent';
-import MediaCard from '../../components/Crads/MediaCard';
 import axios from 'axios';
 
 const styles = theme  => ({
@@ -23,60 +19,33 @@ const styles = theme  => ({
   },
 });
 
-export class GenresBar extends Component {
+class GenresBar extends Component {
   constructor(props){
     super(props);
     this.state = {
-      value: 'heavy-metal',
-      tracks: [],
-      isLoading: false,
-      error: null
+      genres: 'heavy-metal'
     }
   }
   
   componentDidMount() {
-    this.setState({ isLoading: true });
-    this.getData();
+
   }
-
-  getData = () => {
-    const token = 'BQCA6lPMNJ26kZy7Q65CBmL0NaqxvQPPneVFBV36tEUxpkjItpUEekVjb-LYZwMNGxttEQ-CC8Rz_OfsB6ZTR0aCwEifHDGjaR1WU66TlNcdI7zv2BSPF6DJ8psUnpgAzWrS_KHhqi9JgwKEF8i1HeTBeMrJfAB6raZ3i8gIbr_8HlwK6-efAgwQWqrWng';
-    const config = {
-      headers: { 'Authorization': "Bearer " + token }
-    }
-
-    const endPoint = 'https://api.spotify.com/v1/recommendations';
-    const limit = 18;
-    let link = `${endPoint}?limit=${limit}&seed_genres=${this.state.value}`;
-    // console.info(link);
-
-    axios.get(link, config) 
-    .then(result => this.setState({
-      tracks: result.data.tracks,
-      isLoading: false
-    }))
-    .catch(error => this.setState({
-      error: true,
-      isLoading: false
-    }));
-  }
-
+  
   handleChange = (event, value) => {
-    this.setState({ value },() => {
-      this.getData();
-    })
-  };
+    this.setState({ value });
+    console.log('genres Selected:', value)
+    console.log('GenrerBAR')
+
+    this.props.callbackFromParent(value);
+    };
 
     render() {
-      const { classes } = this.props;
-      const { value, tracks, isLoading, error } = this.state;
-      // console.info('track', tracks);
-      // console.info('Genres',value);
+      const { genres } = this.state;
     return (
       <div>
       <AppBar position="static" color="default">
           <Tabs
-            value={value}
+            value={genres}
             onChange={this.handleChange}
             indicatorColor="primary"
             textColor="primary"
@@ -84,24 +53,13 @@ export class GenresBar extends Component {
             scrollButtons="auto"
           >
             <Tab label="Rock" value='heavy-metal' />
-            <Tab label="House" value="deep-house" />
-            <Tab label="Drum and Base" value="drum-and-bass" />
-            <Tab label="Electro" value="electro"/>
-            <Tab label="Pop" value="pop"/>
-            <Tab label="Hip Hop" value="hip-hop"/>
+            <Tab label="House" value='deep-house' />
+            <Tab label="Drum and Base" value='drum-and-bass' />
+            <Tab label="Electro" value='electro'/>
+            <Tab label="Pop" value='pop'/>
+            <Tab label="Hip Hop" value='hip-hop'/>
           </Tabs>
-
         </AppBar>
-        <Grid container
-        className={classes.wrapper}
-        spacing={16}
-        direction='row'
-        alignItems='center'
-        justify='center'>
-        {isLoading ? <CircularProgress className={classes.progress} /> : ''}
-        { error ? <ErrorComponent /> : '' }
-         <MediaCard tracks={tracks}></MediaCard>
-        </ Grid>
       </div>
     )
   }
